3154254ee216af8e14bec72fcb32bccc691c37f5
Veronica Wharton <wharton.veronica@gmail.com>
2017-05-08 10:17:12 -0400

    divided scripts

--- evaluation_d.py
+++ evaluation_d.py
@@ -5,22 +5,12 @@ from fairml import beta, compute_chain, eta, interval_chaining, top_interval
 
 
 def main():
-    # X = np.random.uniform(0, 1, size=(T, k, d))  # 3-axis ndarray
-    # B = beta(k, d, c)  # true parameters. B[i]: params for arm i
-    # Y = np.array([np.diag(X[t].dot(np.transpose(B))) for t in range(T)])
-
-    # cum_regret, avg_regret, final_regret = top_interval(X, Y, k, d, 0.05, T,
-    #                                                     _print_progress=False)
-    # print('Cumulative regret: %f' % cum_regret)
-    # print('Average regret: %f' % avg_regret)
-    # print('Final regret: %f' % final_regret)
-
     c_vals = [1.0, 2.0, 5.0, 10.0]
 
-    # Plot: Varying T (# of rounds)
-    d = 2
+    # Plot: Varying d (confidence)
+    d_vals = range(1, 50)
     k = 2
-    T_vals = range(3, 1000, 10)
+    T = 1000
 
     results = {
         '0': {
@@ -65,14 +55,14 @@ def main():
             v[str(j)] = []
 
     for c in c_vals:
-        for T in T_vals:
+        for d in d_vals:
             cum_regret_tis = []
             avg_regret_tis = []
             final_regret_tis = []
             cum_regret_ics = []
             avg_regret_ics = []
             final_regret_ics = []
-            for i in range(0, 1000):  # 1000 trials
+            for i in range(0, 50):  # 50 trials.
                 X = np.random.uniform(0, 1, size=(T, k, d))
                 B = beta(k, d, c)
                 Y = np.array([np.diag(X[t].dot(np.transpose(B))) for t in range(T)])
@@ -114,21 +104,7 @@ def main():
         plt.legend(handles=[c1, c2, c5, c10])
         plt.xlabel('T (# of rounds)', fontsize=18)
         plt.ylabel(v['ylabel'], fontsize=15)
-        plt.savefig('figures/' + v['name'])
-
-    # Plot: Varying k (# groups)
-    # d = 2
-    # k_vals = range(1, 50)
-    # T = 1000
-    # for c in c_vals:
-    #     for k in k_vals:
-
-    # Plot: Varying d (confidence)
-    # d_vals = range(1, 50)
-    # k = 2
-    # T = 1000
-    # for c in c_vals:
-    #    for d in d_vals:
+        plt.savefig('figures_d/' + v['name'])
 
 
 def mean(numbers):
